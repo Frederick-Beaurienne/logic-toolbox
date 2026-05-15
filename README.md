9c8609b5dca5706915d10dfdefb47689dc3b3b4d
frederick.beaurienne@gmail.com <frederick.beaurienne@gmail.com>
2026-05-15 10:34:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# logic-toolbox
+Spring Boot toolbox implementing logic and data manipulation challenges with clean architecture and unit testing.
---
+++ src/main/java/com/fred/logictoolbox/LogicToolboxApplication.java
@@ -0,0 +1,13 @@
+package com.fred.logictoolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogicToolboxApplication.class, args);
+    }
+
+}
